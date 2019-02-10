e471b18f45a1727dbd100e6999ed56656b7c954d
lampapps <jamesoferry@gmail.com>
2019-02-10 18:22:29 -0500

    Unpaded sync script

--- s3_sync.sh
+++ s3_sync.sh
@@ -11,10 +11,12 @@
 
 AWS_PROFILE='default'
 STAGING_BUCKET='test.paintballreview'
-LIVE_BUCKET=' '
+LIVE_BUCKET='www.paintballreview.info'
 SITE_DIR='_site/'
 REGION='us-east-1'
 CLOUDFRONTID='enterid from cloudfront'
+INDEX_PAGE='index.html'
+ERROR_PAGE='error.html'
 
 
 ##
@@ -67,61 +69,65 @@ if [[ "$1" = "live" ]]; then
     elif [[ "$1" = "staging" ]]; then
     BUCKET=$STAGING_BUCKET
     green 'Deploying to staging bucket'
-
+    # create and configure the bucket to be a website
+    yellow '--> Creating a static website bucket'
+    aws s3api create-bucket --bucket $BUCKET --region $REGION --profile $AWS_PROFILE
 else
     echo "Expected 1 argument, got $#" >&2
     usage
     exit 2
 fi
-#echo $BUCKET
-# create and configure the bucket to be a website
-yellow '--> Creating a static website bucket'
-aws s3api create-bucket --bucket $BUCKET --region $REGION --profile $AWS_PROFILE
-aws s3 website s3://$bucket --index-document index.html --error-document error.html --profile $AWS_PROFILE
 
-#Prepend the path to S3 for the bucket
-BUCKET='s3://'$BUCKET
+
+#echo $BUCKET
+
+aws s3 website $BUCKET --index-document $INDEX_PAGE --error-document $ERROR_PAGE --profile $AWS_PROFILE
 
 # Build the site in the folder
 yellow '--> Running Jekyll build'
 jekyll build
 
-
-#red '--> Gzipping all html, css and js files'
-#find $SITE_DIR \( -iname '*.html' -o -iname '*.css' -o -iname '*.js' \) -exec gzip -9 -n {} \; -exec mv {}.gz {} \;
+#Prepend the path to S3 for the bucket
+BUCKET='s3://'$BUCKET
 
 yellow '--> Uploading css files'
-aws s3 sync $SITE_DIR $BUCKET --exclude '*.*' --include '*.css' --content-type 'text/css' --cache-control 'max-age=604800' --acl public-read --profile $AWS_PROFILE
+aws s3 sync $SITE_DIR $BUCKET --exclude '*.*' --include '*.css' --content-type 'text/css' --cache-control 'max-age=604800' --acl public-read --delete --profile $AWS_PROFILE
 
 
 yellow '--> Uploading js files'
-aws s3 sync $SITE_DIR $BUCKET --exclude '*.*' --include '*.js' --content-type 'application/javascript' --cache-control 'max-age=604800' --acl public-read --profile $AWS_PROFILE
+aws s3 sync $SITE_DIR $BUCKET --exclude '*.*' --include '*.js' --content-type 'application/javascript' --cache-control 'max-age=604800' --acl public-read --delete --profile $AWS_PROFILE
 
 # Sync media files first (Cache: expire in 10weeks)
 yellow '--> Uploading images (jpg, png, ico)'
-aws s3 sync $SITE_DIR $BUCKET --exclude '*.*' --include '*.png' --include '*.jpg' --include '*.ico' --expires 'Sat, 20 Nov 2025 18:46:39 GMT' --cache-control 'max-age=6048000' --acl public-read --profile $AWS_PROFILE
+aws s3 sync $SITE_DIR $BUCKET --exclude '*.*' --include '*.png' --include '*.jpg' --include '*.ico' --expires 'Sat, 20 Nov 2025 18:46:39 GMT' --cache-control 'max-age=6048000' --acl public-read --delete --profile $AWS_PROFILE
 
 
 # Sync html files (Cache: 2 hours)
 yellow '--> Uploading html files'
-aws s3 sync $SITE_DIR $BUCKET --exclude '*.*' --include '*.html' --content-type 'text/html' --cache-control 'max-age=7200, must-revalidate' --acl public-read --profile $AWS_PROFILE
+aws s3 sync $SITE_DIR $BUCKET --exclude '*.*' --include '*.html' --content-type 'text/html' --cache-control 'max-age=7200, must-revalidate' --acl public-read --delete --profile $AWS_PROFILE
 
 
 # Sync everything else
 yellow '--> Syncing everything else'
-aws s3 sync $SITE_DIR $BUCKET --delete --cache-control 'max-age=7200, must-revalidate' --acl public-read --profile $AWS_PROFILE
+aws s3 sync $SITE_DIR $BUCKET --delete --cache-control 'max-age=7200, must-revalidate' --acl public-read --delete --profile $AWS_PROFILE
 
-#Strip the s3:// off the front so next command can run
-BUCKET="${BUCKET:5}"
+if [[ "$1" = "live" ]]; then
+    # Invalidate all objects on Cloudfront forcing the cache to refresh from origin
+    #aws cloundfront create-invalidation -distribution-id $CLOUDFRONTID -paths "/*""
+    green 'Deployed to live bucket'
+    green 'Do not forget to turn on gzip in AWS CLoudfront'
 
-#return the url of the website
-green 'http://'$BUCKET'.s3-website-'$REGION'.amazonaws.com/index.html'
+    elif [[ "$1" = "staging" ]]; then
+    #Strip the s3:// off the front
+    BUCKET="${BUCKET:5}"
+    #return the url of the website
+    green 'http://'$BUCKET'.s3-website-'$REGION'.amazonaws.com/index.html'
+fi
 
 
-# Invalidate all objects on Cloudfrount forcing the cache to refresh from origin
-#aws cloundfront create-invalidation -distribution-id $CLOUDFRONTID -paths "/*""
 
-# aws s3 sync $site_dir s3://$LIVE_BUCKET --storage-class=STANDARD_IA
+
+
 
 
 
